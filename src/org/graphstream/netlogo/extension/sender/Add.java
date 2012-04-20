49342ee48157d996833859d7ede3de5f8ef990db
sbalev <stefan.balev@gmail.com>
2012-04-20 22:43:07 +0200

    Back to the old command names. Still can't decide :)

---
+++ src/org/graphstream/netlogo/extension/sender/Add.java
@@ -0,0 +1,46 @@
+package org.graphstream.netlogo.extension.sender;
+
+import org.graphstream.netlogo.extension.GSManager;
+import org.nlogo.api.Agent;
+import org.nlogo.api.Argument;
+import org.nlogo.api.Context;
+import org.nlogo.api.DefaultCommand;
+import org.nlogo.api.ExtensionException;
+import org.nlogo.api.Link;
+import org.nlogo.api.LogoException;
+import org.nlogo.api.Syntax;
+import org.nlogo.api.Turtle;
+
+public class Add extends DefaultCommand {
+
+	@Override
+	public String getAgentClassString() {
+		return "TL";
+	}
+
+	@Override
+	public Syntax getSyntax() {
+		return Syntax.commandSyntax(new int[] { Syntax.TYPE_STRING });
+	}
+
+	@Override
+	public void perform(Argument[] args, Context context)
+			throws ExtensionException, LogoException {
+		try {
+			String senderId = args[0].getString();
+			GSSender sender = GSManager.getSender(senderId);
+			if (sender == null)
+				return;
+			Agent agent = context.getAgent();
+			if (agent instanceof Turtle)
+				sender.sendNodeAdded(agent.id());
+			else if (agent instanceof Link) {
+				Link link = (Link) agent;
+				sender.sendEdgeAdded(link.end1().id(), link.end2().id(),
+						link.isDirectedLink());
+			}
+		} catch (LogoException e) {
+			throw new ExtensionException(e.getMessage());
+		}
+	}
+}
---
+++ src/org/graphstream/netlogo/extension/sender/Clear.java
@@ -0,0 +1,36 @@
+package org.graphstream.netlogo.extension.sender;
+
+import org.graphstream.netlogo.extension.GSManager;
+import org.nlogo.api.Argument;
+import org.nlogo.api.Context;
+import org.nlogo.api.DefaultCommand;
+import org.nlogo.api.ExtensionException;
+import org.nlogo.api.LogoException;
+import org.nlogo.api.Syntax;
+
+public class Clear extends DefaultCommand {
+
+	@Override
+	public String getAgentClassString() {
+		return "O";
+	}
+
+	@Override
+	public Syntax getSyntax() {
+		return Syntax.commandSyntax(new int[] { Syntax.TYPE_STRING });
+	}
+
+	@Override
+	public void perform(Argument[] args, Context context)
+			throws ExtensionException, LogoException {
+		try {
+			String senderId = args[0].getString();
+			GSSender sender = GSManager.getSender(senderId);
+			if (sender == null)
+				return;
+			sender.sendGraphCleared();
+		} catch (LogoException e) {
+			throw new ExtensionException(e.getMessage());
+		}
+	}
+}
