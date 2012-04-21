af4a32a44c3ef9072fd7cc9852d66bc3b6d6cd79
sbalev <stefan.balev@gmail.com>
2012-04-21 21:22:03 +0200

    A VerboseSink connected to NetStreamReceiver. Very useful.

---
+++ src-test/TestSender.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.graphstream.stream.netstream.NetStreamReceiver;
+import org.graphstream.stream.thread.ThreadProxyPipe;
+import org.graphstream.util.VerboseSink;
+
+
+public class TestSender {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		NetStreamReceiver receiver = new NetStreamReceiver(2012);
+		final ThreadProxyPipe pipe = receiver.getDefaultStream();
+		pipe.addSink(new VerboseSink());
+		
+		while (true) {
+			pipe.pump();
+			Thread.sleep(100);
+		}		
+	}
+
+}
